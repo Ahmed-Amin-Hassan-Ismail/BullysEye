02c096b123f40ca92853c0e85cb074db89167b6a
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2021-12-27 23:47:05 +0200

    initial commit

---
+++ BullysEye/BullysEye/BullysEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullysEyeApp.swift
+//  BullysEye
+//
+//  Created by Ahmed Amin on 27/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct BullysEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
