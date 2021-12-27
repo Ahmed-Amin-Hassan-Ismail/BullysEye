262c033556118e7a6bc6a8b896d3b6e4468648e0
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2021-12-27 23:54:44 +0200

    add textView for to-do list

--- BullysEye/BullysEye/ContentView.swift
+++ BullysEye/BullysEye/ContentView.swift
@@ -9,13 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            VStack {
+                Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN")
+                    .padding()
+                Text("89")
+            }
+            HStack {
+                Text("1")
+                Slider(value: .constant(50.0), in: 1...100)
+                Text("100")
+            }
+            Button("Hit me") {
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView().previewLayout(.fixed(width: 896, height: 414))
     }
 }
